67a30fe232bcd25f28a44aa6357937e8b39ca70a
FredCommo <fredcommo@gmail.com>
2014-03-27 11:52:01 -0700

    new default values and warnings

--- R/convertToProp.R
+++ R/convertToProp.R
@@ -1,6 +1,6 @@
-convertToProp <- function(y, T0 = NA, Ctrl = NA){
-  if(is.na(Ctrl)) Ctrl <- max(y, na.rm = TRUE)
-  if(is.na(T0))
-    return(y/Ctrl)
-  else return((y-T0)/(Ctrl-T0))
+convertToProp <- function(y, T0=min(y, na.rm=TRUE), Ctrl=max(y, na.rm=TRUE)){
+  # If nothing specified: T0 is min(y), Ctrl is max(y)
+  if(is.na(T0) | is.na(Ctrl))
+    warning(call.=FALSE, "Please, provide non-NA T0 and Ctrl values.")
+  return((y-T0)/(Ctrl-T0))
 }
