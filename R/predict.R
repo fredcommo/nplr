ca8ff6eba3b9ddfc3568efafc698801b462c0b06
FredCommo <fredcommo@gmail.com>
2014-03-27 12:43:23 -0700

    y-values have to be in the range defined by bottom and top asymptotes. If not, out-of-range values are replaced + warning

--- R/predict.R
+++ R/predict.R
@@ -2,13 +2,21 @@ setMethod(
   f = "predict", 
   signature = "nplm", 
   definition = function(object, targets, B=1e4){
-    if(any(targets<0 | targets>1))
-      stop("Target values have to be between 0 and 1 (fraction of y)")
     pars <- getPar(object)
+    if(any(targets<=pars$params$bottom)){
+      targets[which(targets<=pars$params$bottom)] <- min(getFitValues(object))
+      warning("Warning: One (or more) of the provided values were below the estimated bottom asymptote. These values have been replaced by the minimal possible value the model can estimates",
+              call.=FALSE)
+    }
+    if(any(targets>=pars$params$top)){
+      targets[which(targets>=pars$params$top)] <- max(getFitValues(object))
+      warning("Warning: One (or more) of the values were above the estimated top asymptote. These values have been replaced by the maximal possible value the model can estimates",
+              call.=FALSE)
+    }
     estim <- lapply(targets, function(target)
       .estimateRange(target, getStdErr(object), pars$params, B, object@useLog)
     )
-    estim <- cbind.data.frame(prop=targets, do.call(rbind, estim))
+    estim <- cbind.data.frame(y=targets, do.call(rbind, estim))
     colnames(estim)[-1] <- c('xmin', 'x', 'xmax')
     return(estim)
   }
