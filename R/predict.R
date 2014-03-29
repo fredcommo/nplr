f387f26314194da9c029e767e49819004e7381d9
FredCommo <fredcommo@gmail.com>
2014-03-29 11:11:59 -0700

    modified warning message

--- R/predict.R
+++ R/predict.R
@@ -5,13 +5,15 @@ setMethod(
     pars <- getPar(object)
     if(any(targets<=pars$params$bottom)){
       targets[which(targets<=pars$params$bottom)] <- min(getFitValues(object))
-      warning("Warning: One (or more) of the provided values were below the estimated bottom asymptote. These values have been replaced by the minimal possible value the model can estimates",
-              call.=FALSE)
+      warning("One (or more) of the provided values were below the estimated bottom asymptote.",
+              call.=FALSE, immediate.=TRUE)
+      message("These values have been replaced by the minimal possible value the model can estimate.")
     }
     if(any(targets>=pars$params$top)){
       targets[which(targets>=pars$params$top)] <- max(getFitValues(object))
-      warning("Warning: One (or more) of the values were above the estimated top asymptote. These values have been replaced by the maximal possible value the model can estimates",
-              call.=FALSE)
+      warning("One (or more) of the values were above the estimated top asymptote.",
+              call.=FALSE, immediate.=TRUE)
+      message("These values have been replaced by the maximal possible value the model can estimate.")
     }
     estim <- lapply(targets, function(target)
       .estimateRange(target, getStdErr(object), pars$params, B, object@useLog)
